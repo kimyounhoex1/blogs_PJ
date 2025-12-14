05cd1b6f9c1ee1f816f349c1a87711d3c2d4ced0
kimyounhoex1 <dordyd11@gmail.com>
2025-12-15 00:02:44 +0900

    ee

--- src/main/java/com/project/blog/domain/member/controller/MemberController.java
+++ src/main/java/com/project/blog/domain/member/controller/MemberController.java
@@ -1,14 +1,12 @@
 package com.project.blog;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MemberController {
 
-	@GetMapping("/")
+    @GetMapping("/")
     public String hello() {
         return "hello";
     }
