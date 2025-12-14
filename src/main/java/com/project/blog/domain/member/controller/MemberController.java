50e747ea179290763ac074abd61d57268ec52fdf
kimyounhoex1 <dordyd11@gmail.com>
2025-12-14 23:27:02 +0900

    tt

--- src/main/java/com/project/blog/domain/member/controller/MemberController.java
+++ src/main/java/com/project/blog/domain/member/controller/MemberController.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@controller
+@Controller
 public class BlogApplication {
 
 	@GetMapping("/")
     public String hello() {
-        return "Hello";
+        return "hello";
     }
 }
